947bae968bd5cdd31c227af54b0f2f2ccf112ae2
Shawn Rodgers <me@bentsignal.com>
2025-12-19 21:02:08 -0500

    clean up auth config

--- packages/convex/src/auth.ts
+++ packages/convex/src/auth.ts
@@ -1,5 +1,4 @@
 import type { GenericCtx } from "@convex-dev/better-auth";
-// import type { BetterAuthOptions } from "better-auth";
 import { expo } from "@better-auth/expo";
 import { createClient } from "@convex-dev/better-auth";
 import { convex } from "@convex-dev/better-auth/plugins";
@@ -7,12 +6,9 @@ import { betterAuth } from "better-auth";
 
 import type { DataModel } from "./_generated/dataModel";
 import { components } from "./_generated/api";
-import { query } from "./_generated/server";
 import authConfig from "./auth.config";
 import { env } from "./convex.env";
 
-// The component client has methods needed for integrating Convex with Better Auth,
-// as well as helper methods for general use.
 export const authComponent = createClient<DataModel>(components.betterAuth);
 
 export const createAuth = (ctx: GenericCtx<DataModel>) => {
@@ -20,10 +16,6 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
     baseURL: env.CONVEX_SITE_URL,
     trustedOrigins: [env.CONVEX_SITE_URL, "ruby://"],
     database: authComponent.adapter(ctx),
-    // Configure simple, non-verified email/password to get started
-    emailAndPassword: {
-      enabled: true,
-    },
     socialProviders: {
       google: {
         prompt: "select_account",
@@ -31,18 +23,6 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
         clientSecret: env.GOOGLE_CLIENT_SECRET,
       },
     },
-    plugins: [
-      // The Expo and Convex plugins are required
-      expo(),
-      convex({ authConfig }),
-    ],
+    plugins: [expo(), convex({ authConfig })],
   });
 };
-// Example function for getting the current user
-// Feel free to edit, omit, etc.
-export const getCurrentUser = query({
-  args: {},
-  handler: async (ctx) => {
-    return authComponent.getAuthUser(ctx);
-  },
-});
