dba4b0d048de1afa76914f78923a8dbccf2a1dc7
Shawn Rodgers <me@bentsignal.com>
2025-12-25 09:45:23 -0500

    update qr code

--- apps/nextjs/src/app/qr/page.tsx
+++ apps/nextjs/src/app/qr/page.tsx
@@ -3,7 +3,7 @@ export default function QRPage() {
     <div className="flex h-screen w-screen items-center justify-center">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="https://ws88jscyfn.ufs.sh/f/iUXo70XEUowpc2ZlKkDs2MK6Ef5nvoQiGyOCkS1eadItRWPY"
+        src="https://ws88jscyfn.ufs.sh/f/iUXo70XEUowpKnikAYqARsBxyQlJVG9bI4Zzt0UpSX5Ce1in"
         alt="QR"
         className="size-92"
       />
