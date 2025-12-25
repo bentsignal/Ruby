f8d9c6ada3203bdde2ffd9314a2ee54774e54517
Shawn Rodgers <me@bentsignal.com>
2025-12-25 00:24:08 -0500

    wrap

--- apps/nextjs/src/app/open/page.tsx
+++ apps/nextjs/src/app/open/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-export default function HomePage() {
+export default function OpenPage() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Link href="/open">
+      <Link href="/qr">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://ws88jscyfn.ufs.sh/f/iUXo70XEUowpaM66b2RTJkgLy0siReFVDBWAOq9Mmxpzfu6l"
+          src="https://ws88jscyfn.ufs.sh/f/iUXo70XEUowp70Qp4fnAYBTgV2J5MNOWEsxKewFP3l9Rpv1S"
           alt="Ruby"
           className="size-92 cursor-pointer"
         />
---
+++ apps/nextjs/src/app/qr/page.tsx
@@ -0,0 +1,12 @@
+export default function QRPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="https://ws88jscyfn.ufs.sh/f/iUXo70XEUowpc2ZlKkDs2MK6Ef5nvoQiGyOCkS1eadItRWPY"
+        alt="QR"
+        className="size-92"
+      />
+    </div>
+  );
+}
